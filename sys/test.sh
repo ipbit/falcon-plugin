44062fd625d2edfd6316ca496f4ee947a4b6080a
Gruipeng <61941597+Gruipeng@users.noreply.github.com>
2020-06-19 10:23:17 +0800

    Create test.sh
    test-20200619

---
+++ sys/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+STEP=60
+
+# push数据到falcon
+function agent_push {
+        METRIC=$1
+        VALUE=$2
+        TAGS=$3
+        TS=$(date -d "`date +'%F %H:%M:00'`" +%s)
+        if [ -f /writable/.deviceID ]
+        then
+                ENDPOINT=`cat /writable/.deviceID`
+        else
+                ENDPOINT=$HOSTNAME
+        fi
+        curl -m 15 -X POST -d "[{\"metric\": \"$METRIC\", \"endpoint\": \"$ENDPOINT\", \"timestamp\": $TS,\"step\": $STEP,\"value\": $VALUE,\"counterType\": \"GAUGE\",\"tags\": \"$TAGS\"}]" http://127.0.0.1:1988/v1/push > 
+/dev/null 2>&1
+}
+
+
+TS=$(date -d "`date +'%F %H:%M:00'`" +%s)
+echo "[{\"endpoint\": \"BJOffice-CPUseal6-188\", \"tags\": \"test=test1\", \"timestamp\": $TS, \"metric\": \"test.test1\", \"value\": 9, \"counterType\": \"GAUGE\", \"step\": 60}, {\"endpoint\": \"BJOffice-CPUseal6-188\", 
+\"tags\": \"test=test2\", \"timestamp\": $TS, \"metric\": \"test.test1\", \"value\": 9, \"counterType\": \"GAUGE\", \"step\": 60}]"
