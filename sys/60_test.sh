11c49e73f71dd1a04b41eae8d58f9f47be65df44
Gruipeng <61941597+Gruipeng@users.noreply.github.com>
2020-06-19 18:10:46 +0800

    Update 60_test.sh

--- sys/60_test.sh
+++ sys/60_test.sh
@@ -19,5 +19,4 @@ function agent_push {
 
 ###
 TS=$(date -d "`date +'%F %H:%M:00'`" +%s)
-echo "[{\"endpoint\": \"BJOffice-CPUseal6-188\", \"tags\": \"test=test1\", \"timestamp\": $TS, \"metric\": \"test.test1\", \"value\": 9, \"counterType\": \"GAUGE\", \"step\": 60}, {\"endpoint\": \"BJOffice-CPUseal6-188\", 
-\"tags\": \"test=test2\", \"timestamp\": $TS, \"metric\": \"test.test1\", \"value\": 9, \"counterType\": \"GAUGE\", \"step\": 60}]"
+echo "[{\"endpoint\": \"bjoffice-cpuseal6-219\", \"tags\": \"test=test1\", \"timestamp\": $TS, \"metric\": \"test.test1\", \"value\": 9, \"counterType\": \"GAUGE\", \"step\": 60}, {\"endpoint\": \"bjoffice-cpuseal6-219\", \"tags\": \"test=test2\", \"timestamp\": $TS, \"metric\": \"test.test1\", \"value\": 9, \"counterType\": \"GAUGE\", \"step\": 60}]"
