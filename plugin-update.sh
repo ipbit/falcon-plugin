8da576b115cd58a4d1232e694787e0f6605f3b74
Gruipeng <61941597+Gruipeng@users.noreply.github.com>
2020-06-22 13:40:44 +0800

    Create plugin-update.sh

---
+++ plugin-update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+
+PLUGIN_DIR=/usr/local/open-falcon/plugin/
+RANDOM_SLEEP=`echo $(($RANDOM%3000))`
+
+
+sleep $RANDOM_SLEEP
+curl http://127.0.0.1:1988/plugin/update
+
+for SCRIPT_LIST in `find $PLUGIN_DIR -name "*" | grep -E "*.sh$|*.py$" `
+do
+        chmod +x $SCRIPT_LIST
+done
