4f79902207dcde192bf4cf5a483716ca62f107b5
Gruipeng <61941597+Gruipeng@users.noreply.github.com>
2020-07-13 18:24:38 +0800

    lotus
     sync_status, workers panic

---
+++ lotus/600_sync_status.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# -1=daemon down, 1-3=sync err number
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+TS=$(date -d "`date +'%F %H:%M:00'`" +%s)
+TAG="type=sync.status"
+METRIC=lotus
+STEP=600
+
+ENDPOINT=`cat /usr/local/open-falcon/agent/config/cfg.json | grep hostname  | awk -F'"' '{print $4}'`
+if [ ! $ENDPOINT ]
+then
+	ENDPOINT=$HOSTNAME
+fi
+
+DAEMON=`kubectl get pods -n lotus | grep lotus-daemon | grep Running | awk '{print $1}' 2> /dev/null `
+VALUE=`kubectl  -n lotus exec -it $DAEMON -- /home/master/lotus sync status | grep "Stage:" | grep -i err | wc -l 2> /dev/null  `
+
+function PRINT_LIST {
+	LIST[0]="{\"endpoint\": \"$ENDPOINT\", \"tags\": \"$TAG\", \"timestamp\": $TS, \"metric\": \"$METRIC\", \"value\": $VALUE, \"counterType\": \"GAUGE\", \"step\": $STEP},"	
+	echo ${LIST[*]} | sed -e 's/{/[{/' -e 's/},$/}]/'
+}
+
+if [ ! $DAEMON ]
+then
+	VALUE=-1
+fi
+
+
+PRINT_LIST
---
+++ lotus/600_workers_list_panic.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#  0=dead,	1=workers panic
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+TS=$(date -d "`date +'%F %H:%M:00'`" +%s)
+TAG="type=workerslist.panic"
+METRIC=lotus
+STEP=600
+TMP=/tmp/lotus_workers_list_panic.tmp
+ENDPOINT=`cat /usr/local/open-falcon/agent/config/cfg.json | grep hostname  | awk -F'"' '{print $4}'`
+if [ ! $ENDPOINT ]
+then
+	ENDPOINT=$HOSTNAME
+fi
+
+
+function PRINT_LIST {
+	LIST[0]="{\"endpoint\": \"$ENDPOINT\", \"tags\": \"$TAG\", \"timestamp\": $TS, \"metric\": \"$METRIC\", \"value\": $VALUE, \"counterType\": \"GAUGE\", \"step\": $STEP},"	
+	echo ${LIST[*]} | sed -e 's/{/[{/' -e 's/},$/}]/'
+	rm -rf $LOCK 2> /dev/null
+	rm -rf $TMP 2> /dev/null
+}
+
+LOCK=/tmp/lotus_workers_list_panic.lock
+if [ -e $LOCK ]
+then
+	VALUE=0
+	PRINT_LIST
+	exit 1
+fi
+
+STM=`kubectl get pods -n lotus | grep lotus-storage | grep Running| awk '{print $1}'`
+if [ ! $STM ]
+then
+	VALUE=0
+	PRINT_LIST
+	exit 1
+fi
+
+echo 1 > $LOCK
+kubectl exec -it $STM -n lotus -- /home/master/lotus-storage-miner workers list >  $TMP 2> /dev/null
+PANIC=`cat $TMP | grep panic | wc -l`
+if [ $PANIC -ge 1 ]
+then
+	VALUE=1
+	PRINT_LIST
+	exit 0
+fi
